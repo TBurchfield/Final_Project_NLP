da9743bdf8b0e13c4b9eaae3e21941987dc2d0f7
Catalina <cvajiac@nd.edu>
2018-10-08 09:46:06 -0400

    i wish our data was just json

--- baseline/baseline.py
+++ baseline/baseline.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+
+from collections import defaultdict
+import json
+
+def parse_comments():
+    for comment in sys.stdin:
+        comment = json.loads(comment)
+        body = comment['body'].split()
+        for word in body
+            yield word, comment['ups'] / len(body)
+
+
+def set_scores():
+    word_score = defaultdict(int)
+    for word, score in parse_comments():
+        word_score[word] += score
+
+    return word_score
+
+
+if __name__ == '__main__':
+    word_score = set_scores()
+    for word, score in word_score.items():
+        print(word, score)
