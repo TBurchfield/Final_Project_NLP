a9397ac43d3c2a23277b909e23d97c0cec5c146e
8bit <tburchfi@nd.edu>
2018-10-11 14:58:19 -0400

    connect dat database

--- baseline/baseline.py
+++ baseline/baseline.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 
 from collections import defaultdict
+import sqlite3
 import json
 
-def parse_comments():
-    for comment in sys.stdin:
-        comment = json.loads(comment)
-        body = comment['body'].split()
-        for word in body
-            yield word, comment['ups'] / len(body)
+#def parse_comments():
+#    for comment in sys.stdin:
+#        comment = json.loads(comment)
+#        body = comment['body'].split()
+#        for word in body:
+#            yield word, comment['ups'] / len(body)
 
 
-def set_scores():
-    word_score = defaultdict(int)
-    for word, score in parse_comments():
-        word_score[word] += score
-
-    return word_score
+#def set_scores():
+#    word_score = defaultdict(int)
+#    for word, score in parse_comments():
+#        word_score[word] += score
+#
+#    return word_score
 
 
 if __name__ == '__main__':
-    word_score = set_scores()
-    for word, score in word_score.items():
-        print(word, score)
+    db = sqlite3.connect('file:../data/database.sqlite?mode=ro', uri=True)
+    #for word, score in word_score.items():
+    #    print(word, score)
