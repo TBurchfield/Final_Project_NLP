cb40e83a188279a0224fc9fb9a66d94b7b61d9dd
8bit <tburchfi@nd.edu>
2018-10-11 21:09:52 -0400

    Hey it actually makes some sense now'

--- baseline/baseline.py
+++ baseline/baseline.py
@@ -11,19 +11,27 @@ n_test = 100
 class Model:
   def __init__(self):
     self.counts = defaultdict(float)
+    self.word_instances = defaultdict(int)
     self.avg_score = 0
+    self.avg_word = None
+  def avg_word_score(self):
+    if self.avg_word != None:
+      return self.avg_word
+    self.avg_word = sum((self.counts[w] / self.word_instances[w] for w in self.counts))/ len(self.word_instances)
+    return self.avg_word
 
 def train(pairs):
   #return model
   model = Model()
   all_scores = 0
   for i, (body, score) in enumerate(pairs):
-    processed = body.translate(translator).lower().split()
+    processed = set(body.translate(translator).lower().split())
     n = len(processed)
     all_scores += score
     for word in processed:
       # Otherwise long comments will be favored
       model.counts[word] += score / n
+      model.word_instances[word] += 1
     if i >= n_train:
       break
   avg = all_scores / n_train
@@ -32,16 +40,18 @@ def train(pairs):
 
 def test(model, pairs):
   # returns (score, bad_score)
-  # because we have five fingers
   differences = 0
   bad_differences = 0
   for i, (body, upvotes) in enumerate(pairs):
     guess = 0
-    processed = body.translate(translator).lower().split()
+    processed = set(body.translate(translator).lower().split())
     n = len(processed)
     for word in processed:
       # Otherwise long comments will be favored
-      guess += model.counts[word] / n
+      if model.word_instances[word] != 0:
+        guess += (model.counts[word] / model.word_instances[word])
+      else:
+        guess += model.avg_word_score()
     denom = abs(guess) + abs(upvotes)
     bad_denom = abs(model.avg_score) + abs(upvotes)
     difference = abs(guess - upvotes) / denom if denom > 0 else 0
