9aae40ec6d60aabd401da04c943154e5234cf754
8bit <tburchfi@nd.edu>
2018-12-11 16:00:24 -0500

    Better error stuff

--- models/Linear.py
+++ models/Linear.py
@@ -6,23 +6,19 @@ import itertools as it
 class Linear:
   def __init__(self, data, n_train):
     try:
-      i_body = data.features.index('body')
+      self.i_body = data.features.index('body')
     except ValueError:
       raise ValueError('pls Don"t do that i need body')
     comments, scores = it.tee(it.islice(data.get_pairs(), 0, n_train))
-    comments = np.array(list(f[i_body] for (i, s, f) in comments))
+    comments = np.array(list(f[self.i_body] for (i, s, f) in comments))
     scores = np.array(list((s for (i, s, f) in scores)))
     self.vectorizer = Vectorizer()
     X = self.vectorizer.fit_transform(comments)
     self.model = LinearRegression(normalize=True).fit(X, scores)
 
   def test(self, data, n_test):
-    try:
-      i_body = data.features.index('body')
-    except ValueError:
-      raise ValueError('pls Don"t do that i need body')
     comments, scores = it.tee(it.islice(data.get_pairs(), 0, n_test))
-    comments = (f[i_body] for (i, s, f) in comments)
+    comments = (f[self.i_body] for (i, s, f) in comments)
     scores = np.array(list((s for (i, s, f) in scores)))
     X = self.vectorizer.transform(comments)
     predict = self.model.predict(X)
